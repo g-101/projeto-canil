1ff872ca8654e99469914789e5528b8d07edd8e2
genesis-g101 <gyreplicant@gmail.com>
2022-06-18 21:04:00 -0300

    config inicial

---
+++ README.md
@@ -0,0 +1,13 @@
+# projeto-canil
+
+### Pré-requisitos globais:
+
+`npm i g nodemon`
+
+### Instalação:
+
+`npm install`
+
+### Para rodar o projeto:
+
+`npm run devStart`
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mustache = require("mustache-express");
+
+const path = require("path");
+
+dotenv.config();
+const app = express();
+
+// TEMPLATE ENGINE
+app.engine("mustache", mustache());
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "/views"));
+
+//STATIC
+app.use(express.static(path.join(__dirname, "/public")));
+
+//ROTAS
+
+
+app.listen(process.env.PORT, () => console.log("http://localhost:3333"));
