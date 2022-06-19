b46a156618fb2d68e572cc7a73a1176d02811eea
genesis-g101 <gyreplicant@gmail.com>
2022-06-19 20:49:01 -0300

    criando o diretorio src e organizando

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.set("view engine", "mustache");
 app.set("views", path.join(__dirname, "/views"));
 
 //STATIC
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 //ROTAS
 app.use(mainRouters)
