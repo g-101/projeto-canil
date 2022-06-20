deefa3fa0c035b9f9d7218e2a4d97ea0e13c755d
genesis-g101 <gyreplicant@gmail.com>
2022-06-20 08:14:15 -0300

    criando o model

--- src/controllers/pageControllers.js
+++ src/controllers/pageControllers.js
@@ -1,43 +1,51 @@
 const createMenuObject = require("../helpers/createMenuObject.js")
+const Pet = require("../models/Pet.js")
 
 exports.home = (req, res) => {
     // res.send("home no controller");
+    const list = Pet.getAll();
     res.render("pages/page", {
         menu: createMenuObject.createMenu("all"),
         banner: {
             title: "Todos os animais",
             background: "allanimals.jpg",
-        }
+        },
+        list
     });
 }
 
 exports.dogs = (req, res) => {
-    
+    const list = Pet.getFromType("dog")
     res.render("pages/page", {
         menu: createMenuObject.createMenu("dog"),
         banner: {
             title: "Todos os cachorros",
             background: "banner_dog.jpg",
-        }
+        },
+        list
     });
 }
 
 exports.cats = (req, res) => {
+    const list = Pet.getFromType("cat")
     res.render("pages/page", {
         menu: createMenuObject.createMenu("cat"),
         banner: {
             title: "Todos os gatos",
             background: "banner_cat.jpg",
-        }
+        },
+        list
     });
 }
 
 exports.fishes = (req, res) => {
+    const list = Pet.getFromType("fish")
     res.render("pages/page", {
         menu: createMenuObject.createMenu("fish"),
         banner: {
             title: "Todos os peixes",
             background: "banner_fish.jpg",
-        }
+        },
+        list
     });
 }
